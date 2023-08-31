225a2af8f8b856847e9347e799f3e183e2421ed0
B VENKATESH <87414605+20a31a05g3@users.noreply.github.com>
2023-08-31 22:45:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let k=document.getElementById(show)
+user="ibm2023sb01055@smartinternz.com"
+password="Yashraj12345"
+let g=HttpLogonCredentials ("https://us1.ca.analytics.ibm.com/bi/?perspective=dashboard&pathRef=.public_folders%2Ffinal%2Bproject%2FNew%2Bdashboard&action=view&mode=dashboard&subView=model0000018a37c46549_00000000", user, password)
+k.textContent="gasss"
